118cf8db0236a129a2b810af80d0a2c20bb001df
cyodaiken <43050529+cyodaiken@users.noreply.github.com>
2020-02-07 14:45:56 -0500

    My teacher is making me do this. Send help.

--- src/main/java/edu/smith/cs/csc212/aquarium/Aquarium.java
+++ src/main/java/edu/smith/cs/csc212/aquarium/Aquarium.java
@@ -42,28 +42,54 @@ public class Aquarium extends GFX {
 	}
 
 	int fish1X = getWidth() + 100;
-	int fish2X = getWidth() + 300;
+	int fish2X = getWidth() - 300;
+	// little red fish 
+	int fish3X = -200;
+	
+	Fish nemo = new Fish(Color.magenta, 250, 250, true, true);
+	Fish marlin = new Fish(Color.orange, 100, 100, false, false);
+	Fish dory = new Fish(Color.pink, 300, 100, true, false);
+	Fish tuna = new Fish(Color.red, 100, 400, false, true);
 
 	@Override
 	public void draw(Graphics2D g) {
 		// Draw the "ocean" background.
 		g.setColor(Color.blue);
 		g.fillRect(0, 0, getWidth(), getHeight());
-
+		
+		nemo.draw(g);
+		
+		marlin.draw(g);
+		dory.draw(g);
+		tuna.draw(g);
 		// Draw the fish!
 		DrawFish.facingLeft(g, Color.yellow, fish1X, 200);
 		// Draw the confused fish!
 		DrawFish.facingRight(g, Color.green, fish2X, 300);
 
 		// What if we wanted this little fish to swim, too?
-		DrawFish.smallFacingLeft(g, Color.red, 200, 100);
+		DrawFish.smallFacingRight(g, Color.red, fish3X, 100);
 
+		
 		// Draw our snail!
 		algorithm.draw(g);
 
 		// Move the fish!
 		fish1X -= 1;
-		fish2X -= 2;
+		fish2X += 2;
+		fish3X += 2;
+		
+		if (fish1X > getWidth() + 100) {
+			fish1X = 100;
+		}
+		
+		if (fish2X > getWidth() + 100) {
+			fish2X = -100;
+		}
+		
+		if (fish3X > getWidth() + 100) {
+			fish3X = -100;
+		}
 	}
 
 	public static void main(String[] args) {

4361a5432c8b3b2f959e3b8f179875c8a38b1ac2
cyodaiken <43050529+cyodaiken@users.noreply.github.com>
2020-02-07 17:40:21 -0500

    commit 2

--- src/main/java/edu/smith/cs/csc212/aquarium/Fish.java
+++ src/main/java/edu/smith/cs/csc212/aquarium/Fish.java
@@ -2,6 +2,7 @@ package edu.smith.cs.csc212.aquarium;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.util.Random;
 
 public class Fish {
 	// Every fish has an x which is an int 
@@ -20,26 +21,50 @@ public class Fish {
 		this.y = startY;
 		this.isLittle = isLittle;
 		this.facingLeft = facingLeft;
-		this.destX = 450;
-		this.destY = 450;
+		
+		Random random = new Random();
+		this.destX = random.nextInt(500);
+		this.destY = random.nextInt(500);
 	}
 	
 	public void swim() {
-		
-		if (this.x < this.destX) {
-			this.x += 1;
-	}
-		
-		if (this.y < this.destY) {
-				this.y += 1;
+			
+		if (this.facingLeft) {
+			if (this.x < this.destX) {
+				this.x -= 1;
+				if (this.x == 0) {
+					this.x = 500;
+				}
+			} 
+		} else {
+			if(this.x < this.destX) {
+				this.x += 1;
+				if (this.x == 500) {
+					this.x = 0;
+				}
+			} 
 		}
+
+		if (this.y < this.destY) {
+			// if (Math.abs(this.y - this.destY) < 10) {
+		// somehow pick a new destination}
+			this.y += 1;
+		} else if (this.y > this.destY) {
+			this.y -= 1;
+		} // else choose new destination
+		
+		
+		
 	}
 	
 	public void draw(Graphics2D g) {
-		this.swim();
+		
 		// assumes fish face left and are small so add boolean
 		if (isLittle && facingLeft) {	
 			DrawFish.smallFacingLeft(g, this.color, this.x, this.y);
+			/*
+			 * if (this.x < 0) { DrawFish.smallFacingLeft(g, this.color, 500, this.y); }
+			 */
 		} else if (isLittle && !facingLeft) {
 			DrawFish.smallFacingRight(g, this.color, this.x, this.y);
 			
@@ -48,6 +73,8 @@ public class Fish {
 		} else {
 			DrawFish.facingRight(g, this.color, this.x, this.y);
 		}
+		
+		this.swim();
 		
 	}
 
